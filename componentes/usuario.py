81fa30bebcc2814976b45bc59dca9d5788d1bdfc
Nasabun Cyntia 119744 <nasabunc@gmail.com>
2024-06-29 11:59:03 -0300

    Nasabunc

--- componentes/usuario.py
+++ componentes/usuario.py
@@ -1,3 +1,3 @@
 
 
-#campos = ('email', 'password', 'fecha')
+#campos = ('email', 'password', 'fecha') deberia tener los metodos mostrar,actualizar eliminar como en la clase producto, esto es por cada tabla de la BD, una clase por cada tabla.
