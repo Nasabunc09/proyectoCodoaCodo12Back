46ee8d719a25c59b8a6d945de4b4d543339a5600
Daniela Homobono <110701158+danielaHomobono@users.noreply.github.com>
2024-07-02 16:26:28 -0300

    sss

--- componentes/modelos.py
+++ componentes/modelos.py
@@ -1,28 +1,24 @@
-# Clases que corresponden a entidades en la BBDD
 from base_db.tabla_db import Tabla
 from base_db.conexion_db import conexion as con
 from auxiliares.cifrado import encriptar
 
 class Producto(Tabla):
-    
     tabla = 'producto'
-    conexion = con #conexion de la clase nivel, y 'con' que viene de la conexion a la bd de la clase nivel
+    conexion = con
     campos = ('id', 'nombre', 'descripcion', 'stock', 'precio_venta', 'fecha')
     
     def __init__(self, *args, de_bbdd=False):
         super().crear(args, de_bbdd)
-        
+
 class Domicilio(Tabla):
-    
     tabla = 'domicilio'
     conexion = con
     campos = ('id', 'idUsuario', 'calle', 'provincia', 'ciudad', 'numero', 'pais', 'codigoPostal')
     
     def __init__(self, *args, de_bbdd=False):
         super().crear(args, de_bbdd)
-        
+
 class Carrito(Tabla):
-    
     tabla = 'carrito'
     conexion = con
     campos = ('id', 'idUsuario', 'idProducto', 'cantidad')
@@ -31,7 +27,6 @@ class Carrito(Tabla):
         super().crear(args, de_bbdd)
 
 class Orden(Tabla):
-    
     tabla = 'orden'
     conexion = con
     campos = ('id', 'idUsuario', 'idMetodoPago', 'total', 'fecha')
@@ -40,7 +35,6 @@ class Orden(Tabla):
         super().crear(args, de_bbdd)
 
 class Orden_Detalle(Tabla):
-    
     tabla = 'orden_detalle'
     conexion = con
     campos = ('id', 'idOrden', 'idProducto', 'cantidad', 'precio')
@@ -49,7 +43,6 @@ class Orden_Detalle(Tabla):
         super().crear(args, de_bbdd)
 
 class C_Metodo_Pago(Tabla):
-    
     tabla = 'c_metodo_pago'
     conexion = con
     campos = ('id', 'nombre')
@@ -58,7 +51,6 @@ class C_Metodo_Pago(Tabla):
         super().crear(args, de_bbdd)
         
 class Imagen(Tabla):
-    
     tabla = 'imagen'
     conexion = con
     campos = ('id', 'idProducto', 'url_img', 'texto_alt')
@@ -67,7 +59,6 @@ class Imagen(Tabla):
         super().crear(args, de_bbdd)
         
 class Persona(Tabla):
-    
     tabla = 'persona'
     conexion = con
     campos = ('id', 'idUsuario', 'idDomicilio','nombre', 'apellido', 'dni', 'telefono')
@@ -75,6 +66,32 @@ class Persona(Tabla):
     def __init__(self, *args, de_bbdd=False):
         super().crear(args, de_bbdd)
         
+class Usuario(Tabla):
+    tabla = 'usuario'
+    conexion = con
+    campos = ('id', 'email', 'password', 'fecha')
+    
+    def __init__(self, *args, de_bbdd=False):
+        if not de_bbdd:
+            cuenta = []
+            cuenta.append(args[0])
+            cuenta.append(encriptar(args[1]))
+            super().crear(tuple(cuenta), de_bbdd)
+        else:
+            super().crear(args, de_bbdd)
+
+    @classmethod
+    def autenticar(cls, email, password):
+        hashed_password = encriptar(password)
+        consulta = f"SELECT * FROM {cls.tabla} WHERE email = %s AND password = %s"
+        #resultado = cls.__conectar(consulta, (email, hashed_password))
+        resultado = cls._Tabla__conectar(consulta, (email, hashed_password))
+        if resultado:
+            return resultado[0]
+        return None
+
+
+"""       
 class Usuario(Tabla):
     
     tabla = 'usuario'
@@ -90,3 +107,11 @@ class Usuario(Tabla):
             super().crear(tuple(cuenta), de_bbdd)
         else:
             super().crear(args, de_bbdd)
+        @classmethod
+        def autenticar(cls, email, password):
+            hashed_password = encriptar(password)
+            consulta = f"SELECT * FROM {cls.tabla} WHERE email = %s AND password = %s"
+            resultado = cls.__conectar(consulta, (email, hashed_password))
+            if resultado:
+               return resultado[0]
+            return None         """ 
